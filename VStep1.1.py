b4916e0205e012aa27b3652daaea8517b9ed826e
Keene <ZHAP041@live.rhul.ac.uk>
2022-11-11 16:47:02 +0000

    Updated VStep function to ask for user inputs

--- VStep1.1.py
+++ VStep1.1.py
@@ -3,25 +3,32 @@ import matplotlib.pyplot as plt
 
 #Function variables
 
-x_array = np.linspace(-5, 5, 500)
+
+x1 = int(input("What is your potential?\n"))
+x2 = float(input("What is the position of the step in the x axis??\n"))
+
+
+
+
+x_array = np.linspace(-x2, x1, 500)
 V_x = 0
 
-#defining the function for the potential  where V_0 is the potential of the step and a is its position on the x axis
+#defining the function for the potential  where V0 is the potential of the step and a is its position on the x axis
 V = []
-V_0 = int(input("What is your potential?\n"))
+V0 = int(input("What is your potential?\n"))
 a = float(input("What is the position of the step in the x axis??\n"))
 
-def VStep():
+def VStep(V_0, a, x_array):
     for i in range(500): 
      if x_array[i] > a :
-        V.append(V_0)
+        V.append(V0)
      else:
         V.append(0)        
     return V
 
 
 #example plot of the potential 
-plt.plot(x_array, VStep())
+plt.plot(x_array, VStep(V0, a, x_array))
 
     
 
